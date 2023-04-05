7d53e098fddcd01eb7adb4bd08e1d11c8cad1634
Chlupus <purkertovam@gmail.com>
2023-04-05 17:16:24 +0200

    menu correction

--- Program.cs
+++ Program.cs
@@ -4,80 +4,83 @@ Turnstile turnstile = new Turnstile();
 
 bool wantContinue = true;
 
-Console.WriteLine("Choose one of the options");
-Console.WriteLine("1 - Create a new random ticket");
-Console.WriteLine("2 - Create a new specific ticket");
-Console.WriteLine("3 - Show tickets");
-Console.WriteLine("4 - Delete a ticket");
-Console.WriteLine("5 - Convert to CSV");
-Console.WriteLine("6 - Delete all tickets");
-Console.WriteLine("7 - Sort the tickets");
-Console.WriteLine("8 - End program");
-
-int option = int.Parse(Console.ReadLine());
-
-int createTicket = 0;
-int deleteTicket = 0;
-
-switch (option)
+do
 {
-    case 1:
-        turnstile.CreateTicket();
-        Console.Clear();
-        break;
-    case 2:
-        Console.WriteLine("Choose a number of the ticket you want to create:");
+    Console.WriteLine("Choose one of the options");
+    Console.WriteLine("1 - Create a new random ticket");
+    Console.WriteLine("2 - Create a new specific ticket");
+    Console.WriteLine("3 - Show tickets");
+    Console.WriteLine("4 - Delete a ticket");
+    Console.WriteLine("5 - Convert to CSV");
+    Console.WriteLine("6 - Delete all tickets");
+    Console.WriteLine("7 - Sort the tickets");
+    Console.WriteLine("8 - End program");
 
-        try
-        {
-            createTicket = int.Parse(Console.ReadLine());
-        }
-        catch
-        {
-            Console.WriteLine("You must input an integer.");
-        }
+    int option = int.Parse(Console.ReadLine());
 
-        turnstile.CreateTicket(createTicket);
-        Console.ReadKey();
-        Console.Clear();
-        break;
+    int createTicket = 0;
+    int deleteTicket = 0;
 
-    case 3:
-        turnstile.WriteOutTickets();
-        Console.ReadKey();
-        Console.Clear();
-        break;
+    switch (option)
+    {
+        case 1:
+            turnstile.CreateTicket();
+            Console.Clear();
+            break;
+        case 2:
+            Console.WriteLine("Choose a number of the ticket you want to create:");
 
-    case 4:
-        Console.WriteLine("Choose a number of the ticket you want to delete");
-        deleteTicket = int.Parse(Console.ReadLine());
-        turnstile.DeleteTicket(deleteTicket);
-        Console.ReadKey();
-        Console.Clear();
-        break;
-    case 5:
-        turnstile.ConvertToCSV();
-        Console.ReadKey();
-        Console.Clear();
-        break;
-    case 6:
-        turnstile.DeleteTickets();
-        Console.WriteLine("All tickets have been removed.");
-        Console.ReadKey();
-        Console.Clear();
-        break;
-    case 7:
-        turnstile.OrderTickets();
-        turnstile.WriteOutTickets();
-        Console.ReadKey();
-        Console.Clear();
-        break;
-    case 8:
-        wantContinue = false;
-        Console.ReadKey();
-        Console.Clear();
-        break;
-    default:
-        Console.WriteLine("Wrong option");
-        break;
-} while (wantContinue == true) ;
+            try
+            {
+                createTicket = int.Parse(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("You must input an integer.");
+            }
+
+            turnstile.CreateTicket(createTicket);
+            Console.ReadKey();
+            Console.Clear();
+            break;
+
+        case 3:
+            turnstile.WriteOutTickets();
+            Console.ReadKey();
+            Console.Clear();
+            break;
+
+        case 4:
+            Console.WriteLine("Choose a number of the ticket you want to delete");
+            deleteTicket = int.Parse(Console.ReadLine());
+            turnstile.DeleteTicket(deleteTicket);
+            Console.ReadKey();
+            Console.Clear();
+            break;
+        case 5:
+            turnstile.ConvertToCSV();
+            Console.ReadKey();
+            Console.Clear();
+            break;
+        case 6:
+            turnstile.DeleteTickets();
+            Console.WriteLine("All tickets have been removed.");
+            Console.ReadKey();
+            Console.Clear();
+            break;
+        case 7:
+            turnstile.OrderTickets();
+            turnstile.WriteOutTickets();
+            Console.ReadKey();
+            Console.Clear();
+            break;
+        case 8:
+            wantContinue = false;
+            Console.ReadKey();
+            Console.Clear();
+            break;
+        default:
+            Console.WriteLine("Wrong option");
+            break;
+    }
+} while (wantContinue == true);
--- Turnstile.cs
+++ Turnstile.cs
@@ -2,11 +2,108 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace WaterPark
 {
     internal class Turnstile
     {
+        public List<Ticket> listOfTickets;
+        public int ticketId;
+        public Turnstile()
+        {
+            ticketId = 1;
+            listOfTickets = new List<Ticket>();
+        }
+
+        public int CreateTicket()
+        {
+            bool check  = true;
+            Random rndTicket = new Random();
+            int randomId = 0;
+
+            do
+            {
+                randomId = rndTicket.Next(1,10);
+                check = CheckOfExistingTickets(randomId);
+
+            } while (check == true);
+
+
+            Ticket ticket = new Ticket(randomId);
+            listOfTickets.Add(ticket);
+            return ticket.ID;
+        }
+
+        public int CreateTicket(int ticketId)
+        {
+            if (CheckOfExistingTickets(ticketId) == false)
+            {
+                Ticket ticket = new Ticket(ticketId);
+                listOfTickets.Add(ticket);
+                return ticket.ID;
+            }
+
+            else
+            {
+                Console.WriteLine("This ID is already in use.");
+                return -1;
+            }
+        }
+
+        private bool CheckOfExistingTickets(int ticketId)
+        {
+            foreach (var item in listOfTickets)
+            {
+                if (item.ID == ticketId)
+                    return true;
+            }
+            return false;
+        }
+
+        public void WriteOutTickets()
+        {
+            foreach (var item in listOfTickets)
+                Console.WriteLine("Ticket number is: " + item.ID);
+        }
+
+        public void DeleteTicket (int idTicket)
+        {
+            foreach (var item in listOfTickets)
+            {
+                if (item.ID == idTicket)
+                {
+                    listOfTickets.Remove(item);
+                    break;
+                }
+            }
+        }
+
+        public void DeleteTickets()
+        {
+            listOfTickets.Clear();
+        }
+
+        public void ConvertToCSV()
+        {
+            try
+            {
+                var json = JsonSerializer.Serialize(listOfTickets);
+                Console.WriteLine(json);
+                Console.WriteLine("The process succeeded.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Something went wrong. Error: " + e);
+            }
+        }
+
+        public void OrderTickets()
+        {
+            var sortedList = listOfTickets.OrderBy(q => q.ID).ToList();
+            listOfTickets = sortedList;
+        }
     }
 }
